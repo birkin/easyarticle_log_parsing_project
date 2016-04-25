e5b925c9f43899eb29af11c715faeb6aad16f625
birkin <birkin.diana@gmail.com>
2016-04-25 15:41:43 -0400

    refactors line-processing into separate function.

--- easyarticle_log_parser.py
+++ easyarticle_log_parser.py
@@ -20,29 +20,46 @@ class Parser(object):
     def gulp( self, APACHE_LOG_FILE_PATH=None ):
         """ Opens log-file, creates list of dcts for entries containing 'easyarticle'
             Called by parse_log() """
-        ( log_data_lst, filepath, line_parser ) = self.get_gulp_vars( APACHE_LOG_FILE_PATH )
+        ( url_lst, extracted_lst, discounted_lst, filepath, line_parser ) = self.get_gulp_vars( APACHE_LOG_FILE_PATH )
         with open( filepath, 'r' ) as f:
             lines = f.readlines()
             logging.debug( 'found `{}` lines'.format(len(lines)) )
         for (i, line) in enumerate( lines ):
             if i%1000 == 0:
                 logging.debug( 'processed `{}` lines'.format(i) )
-            if 'easyarticle' in line:
-                line_data=line_parser( line )
-                log_data_lst.append( line_data )
-        return log_data_lst
+            self.process_line( line, line_parser, url_lst, extracted_lst, discounted_lst )
+        output_dct = {
+            'url_lst': url_lst, 'url_lst_count': len(url_lst), 'extracted_lst': extracted_lst, 'extracted_lst_count': len(extracted_lst), 'discounted_lst': discounted_lst, 'discounted_lst_count': len(discounted_lst) }
+        return output_dct
 
     def get_gulp_vars( self, APACHE_LOG_FILE_PATH=None, pattern=None ):
         """ Initializes vars.
             Called by gulp() """
-        log_data_lst = []
+        url_lst = []
+        extracted_lst = []
+        discounted_lst = []
         if APACHE_LOG_FILE_PATH is None:
             APACHE_LOG_FILE_PATH = self.APACHE_LOG_FILE_PATH
         if pattern is None:
             pattern = self.APACHE_COMBINED_PATTERN
         line_parser=apache_log_parser.make_parser( pattern )
         logging.debug( 'path, ```{}```'.format(APACHE_LOG_FILE_PATH) )
-        return ( log_data_lst, APACHE_LOG_FILE_PATH, line_parser )
+        return ( url_lst, extracted_lst, discounted_lst, APACHE_LOG_FILE_PATH, line_parser )
+
+    def process_line( self, line, line_parser, url_lst, extracted_lst, discounted_lst ):
+        """ Process logic of line-check.
+            Called by gulp() """
+        if 'easyarticle' in line:
+            line_data=line_parser( line )
+            if 'easyarticle' in line_data.get('request_url_path', ''):
+                if 'login' not in line_data.get('request_url_path', ''):
+                    if len(line_data.get('request_url_query', '')) > 0:  # if there's a good path and querystring...
+                        extracted_lst.append( line_data )  # grab it
+                        if line_data['request_url_query'] not in url_lst:
+                            url_lst.append( line_data['request_url_query'] )
+            else:
+                discounted_lst.append( line_data )  # otherwise save it for a cursory look
+        return
 
     # end class Parser
 
@@ -50,9 +67,11 @@ class Parser(object):
 def parse_log():
     """ Calls parser. """
     prsr = Parser()
-    log_data_lst = prsr.gulp()
-    logging.debug( 'len, log_data_lst, `{}`'.format(len(log_data_lst)) )
-    logging.debug( 'first entry, ```{}```'.format(pprint.pformat(log_data_lst[0])) )
+    output_dct = prsr.gulp()
+    # logging.debug( 'first entry, ```{}```'.format(pprint.pformat(extracted_lst[0])) )
+    # logging.debug( 'entries, ```{}```'.format(pprint.pformat(output_dct)) )
+    logging.debug( 'entries, ```{}```'.format(pprint.pformat(output_dct['url_lst_count'])) )
+    logging.debug( 'entries, ```{}```'.format(pprint.pformat(output_dct['url_lst'])) )
 
     # end def parse_log()
 
