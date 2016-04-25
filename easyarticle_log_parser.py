c1ca5957bddf03a4d1d7caa4848f384cc816770d
birkin <birkin.diana@gmail.com>
2016-04-25 12:48:33 -0400

    enables logging.

--- easyarticle_log_parser.py
+++ easyarticle_log_parser.py
@@ -6,34 +6,46 @@ import glob, logging, os, pprint
 import apache_log_parser
 
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s', datefmt='%d/%b/%Y %H:%M:%S' )
+
+
 class Parser(object):
 
     def __init__(self):
-        self.LOG_FILE_PATH = os.environ['APCH_PRSLG__LOG_PATH']
+        self.APACHE_LOG_FILE_PATH = os.environ['APCH_PRSLG__LOG_PATH']
         self.APACHE_COMBINED_PATTERN = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
 
-    def gulp( self, log_file_path=None, pattern=None ):
-        """ import and parse log files """
-        ( log_data_lst, log_file_path, pattern ) = self.get_gulp_vars( log_file_path, pattern )
+    def gulp( self, APACHE_LOG_FILE_PATH=None, pattern=None ):
+        """ Imports and parses log files.
+            Called by parse_log() """
+        ( log_data_lst, APACHE_LOG_FILE_PATH, pattern ) = self.get_gulp_vars( APACHE_LOG_FILE_PATH, pattern )
         line_parser=apache_log_parser.make_parser( pattern )
-        paths = glob.glob( log_file_path )
-        pprint.pprint( 'paths, ```{}```'.format(pprint.pprint(paths)) )
-        for file_name in paths:
-            logging.info( "file_name: %s" % file_name )
-            file = open( file_name, 'r' )
+        paths = glob.glob( APACHE_LOG_FILE_PATH )
+        # pprint.pprint( 'paths, ```{}```'.format(pprint.pformat(paths)) )
+        logging.debug( 'paths, ```{}```'.format(pprint.pformat(paths)) )
+        for filepath in paths:
+            logging.debug( 'filepath: `{}`'.format(filepath) )
+            file = open( filepath, 'r' )
             lines = file.readlines()
             file.close()
-            logging.info(" read %s lines" % len(lines))
+            logging.debug( 'found `{}` lines'.format(len(lines)) )
             for line in lines:
                 line_data=line_parser( line )
                 log_data_lst.append( line_data )
-        logging.info( "total number of events parsed: %s" % len(log_data_lst) )
         return log_data_lst
 
-    def get_gulp_vars( self, log_file_path, pattern ):
+    def get_gulp_vars( self, APACHE_LOG_FILE_PATH=None, pattern=None ):
+        """ Initializes vars.
+            Called by gulp() """
         log_data_lst = []
-        if log_file_path is None:
-            log_file_path = self.LOG_FILE_PATH
+        if APACHE_LOG_FILE_PATH is None:
+            APACHE_LOG_FILE_PATH = self.APACHE_LOG_FILE_PATH
         if pattern is None:
             pattern = self.APACHE_COMBINED_PATTERN
-        return ( log_data_lst, log_file_path, pattern )
+        return ( log_data_lst, APACHE_LOG_FILE_PATH, pattern )
+
+    # end class Parser
+
+
